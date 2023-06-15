2b61a93acef5db0585725955a83ef30b75294dde
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2023-06-15 23:49:53 +0530

    today's 15-6-23

---
+++ CountInversion.java
@@ -0,0 +1,20 @@
+// import java.util.* ;
+// import java.io.*; 
+public class CountInversion {
+    public static long getInversions(long arr[], int n) {
+        // Write your code here.
+        int ans=0;
+
+        for(int i=0;i<n-1;i++){
+
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]) ans++;
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
