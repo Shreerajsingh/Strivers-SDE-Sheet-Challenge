bb4b697704c4307c12536c9bd144500132ff6278
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2023-06-25 22:14:59 +0530

    Date:  25.06.2023

---
+++ FindMinimumCoins.java
@@ -0,0 +1,18 @@
+// import java.util.* ;
+// import java.io.*; 
+public class FindMinimumCoins
+{
+    public static int findMinimumCoins(int amount)
+    {
+        int count = 0;
+        int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+        int n = coins.length-1;
+        for(int i=n;i>=0;i--){
+            while(amount >= coins[i]){
+                amount -= coins[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
